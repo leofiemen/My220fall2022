a89d85fbeb8d30a686d3489c6259c410b12c72f0
leofiemen <112003931+leofiemen@users.noreply.github.com>
2022-09-05 02:42:00 -0400

    changed app.js script

--- app.js
+++ app.js
@@ -1,38 +1,29 @@
 function setup() {
-    createCanvas(600, 600);
-    angleMode(DEGREES);
+    createCanvas(400, 400);
   }
   
   function draw() {
-  
-    //Set BackGround Color of Canvas
-    background(245);
-        
-    //Draw FACE
-    noStroke();
-    fill(0,0,255);
-    var x = width/2;
-    var y = height/2;
-    ellipse(x,y,x,y);
-  
-    //Draw EYE
-    var trans = 0.15*x;
-    var eye_width = 0.1*x;
-    var eye_height = 0.2*x;
-    fill(0);
-      //Left Eye
-      ellipse(x-trans, y-trans, eye_width, eye_height);
-      //Right Eye
-      ellipse(x+trans, y-trans, eye_width, eye_height);
-  
-    //Draw Smile
-    var smile_size = 0.7*x;
-    var start = 15;
-    var end = 165;
-    stroke(0);
-    strokeWeight(10);
-    noFill();
-    arc(x, y, smile_size, smile_size, start, end);
+    background(220);
+        happyFace(width/2, height/2, 170);
   }
   
-  
+  function happyFace (x, y, diam) {
+        // Face
+        fill(255, 255, 0);
+        stroke(0);
+        strokeWeight(2);
+        ellipse(x, y, diam, diam);
+        
+        // Smile
+        var startAng = .1*PI
+        var endAng = .9*PI
+        var smileDiam = .6*diam;
+        arc(x, y, smileDiam, smileDiam, startAng, endAng);
+        
+        // Eyes
+        var offset = .2*diam;
+        var eyeDiam = .1*diam;
+        fill(0);
+        ellipse(x-offset, y-offset, eyeDiam, eyeDiam);
+        ellipse(x+offset, y-offset, eyeDiam, eyeDiam);
+  }
